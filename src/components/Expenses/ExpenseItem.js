f12a6b975c9e3b222a8d5c282459ae0a08a27b20
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2023-01-04 23:18:37 +0200

    added Add Item to state func, added filter func

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
-import { useState } from "react";
 
 const ExpenseItem = ({props}) => {
-	const [title, setTitle] = useState(props.title);
-	console.log('evaluated by react', props.id)
-
-
-	const clickHandler = () => {
-		setTitle(`Updated ${props.id}`)
-		console.log('updated', props.id)
-	}
-
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate props={props.date} />
 
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">$ {props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,30 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2023')
+    console.log(filteredYear)
+
+    const filterChangeHandler = (year) => {
+        setFilteredYear(year)
+    }
+    // console.log(props.expenses.filter(x => x.date.getFullYear().toString() === filteredYear))
+    // props.expenses.forEach(el => {
+    //     console.log(el.date.getFullYear().toString())
+    // })
     return (
-        <Card className='expenses'>
-            {props.expenses.map(x => <ExpenseItem key={x.id} props={x}/>)}
-        </Card>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {/* {props.expenses.map(x => <ExpenseItem key={x.id} props={x} />)} */}
+                {props.expenses
+                    .filter(x => x.date.getFullYear().toString() === filteredYear)
+                    .map(x => <ExpenseItem key={x.id} props={x} />)
+                }
+            </Card>
     );
 }
 
